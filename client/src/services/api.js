8f4bf2153652ef1e3441c937379b86f1b7e19d21
ozkancondek <ozkncndek@gmail.com>
2022-05-06 19:48:22 +0200

    Fetching data modified

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,41 +1,43 @@
 import axios from "axios";
+import { API } from "./axiosInstance";
+
+const base_url = process.env.REACT_APP_BASE_URL;
+console.log(base_url);
 
 export const fetchData = async (pageNum) => {
   if (pageNum) {
-    let res = await axios(
-      "http://localhost:5000/api/cities/" + `?page=${pageNum}`
-    );
+    let res = await API("/api/cities/" + `?page=${pageNum}`);
     return res.data;
   } else {
-    let res = await axios("http://localhost:5000/api/cities/");
+    let res = await API("/api/cities/");
     return res.data;
   }
 };
 
 export const fetchSingleData = async (id) => {
-  let res = await axios(`http://localhost:5000/api/cities/details/${id}`);
+  let res = await API(`/api/cities/details/${id}`);
   return res;
 };
 
 export const register = async (data) => {
-  let res = await axios.post("http://localhost:5000/api/auth/register", data);
+  let res = await API.post("/api/auth/register", data);
   return res.data;
 };
 
 export const login = async (data) => {
-  let res = await axios.post("http://localhost:5000/api/auth/login", data);
+  let res = await API.post("/api/auth/login", data);
   return res;
 };
 export const newPlace = async (data) => {
-  let res = await axios.post("http://localhost:5000/api/city/newplace", data);
+  let res = await API.post("/api/city/newplace", data);
   return res;
 };
 export const sendComment = async (data) => {
-  let res = await axios.post("http://localhost:5000/api/city/addcomment", data);
+  let res = await API.post("/api/city/addcomment", data);
   return res;
 };
 export const getComments = async () => {
-  let res = await axios(`http://localhost:5000/api/city/comments/`);
+  let res = await API(`/api/city/comments/`);
   return res;
 };
 
